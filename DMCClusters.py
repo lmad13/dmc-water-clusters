Updated the HOHOH ground state to do multiple runs and averaged. Debugged DMC to use correct initial population

DMCClusters.py
@@ -27,12 +27,12 @@ class wavefunction:
         self.set_dtau()
         self.mass=self.molecule.get_mass()
         self.sigma_dx=(2.0000*self.D*self.dtau/self.mass)**0.5
-        self.alpha=0.25/self.dtau
+        self.alpha=0.1/self.dtau
 
         self.recrossing=True if 'node' in potentialName else False
 
     def set_dtau(self):
-        self.dtau=5.0
+        self.dtau=100.0
         print 'dtau is ', self.dtau, 'imaginary atomic time units'
 
     def loadDict(self,fileName):
@@ -51,8 +51,8 @@ class wavefunction:
 
         return dx
 
-    def propagate(self,x,nSteps,setV_ref=False,ConstantV_ref=0,printCensus=False,initialPop=0):
-        print 'ready to propagate for',nSteps, 'steps on x (shaped:',x.shape,')'
+    def propagate(self,x,nSteps,setV_ref=False,ConstantV_ref=0,printCensus=True,initialPop=0):
+        print 'ready to propagate for',nSteps, 'steps on x (shaped:',x.shape,') '
         if initialPop==0:#if it is the default value of zero...it needs to be set.
             initialPop=x.shape[0]
         if setV_ref:
@@ -60,19 +60,19 @@ class wavefunction:
         else:
             v_ref=np.average(self.molecule.V(x))#+(self.alpha*(1-float(N_size_step)/float(nSize)))
 
-        self.currentPop=initialPop
+        self.currentPop=x.shape[0]
         population=[]
         population.append(self.currentPop)
-        print 'and x.size is', initialPop
+        print 'and x.size is', self.currentPop, 'out of',initialPop
 
         vRefList=[]
         vRefList.append(v_ref)
 
-        descendants=np.zeros((initialPop))
-        whoYaFrom=np.arange(initialPop)
+        descendants=np.zeros((self.currentPop))
+        whoYaFrom=np.arange(self.currentPop)
         
         for step in range(nSteps):
-            print 'step #',step
+            if printCensus: print 'step #',step, 
             dx=self.diffuse()
             x=x+dx
             v=self.molecule.V(x)
@@ -86,7 +86,7 @@ class wavefunction:
             mask_survive=(Diff>0)
             nDeaths=np.sum(np.array(Diff<0).astype(int))
             survivors=x[mask_survive]
-            if printCensus: print 'Census: Deaths:',nDeaths,
+            if printCensus: print 'Census: Current Population:',self.currentPop,'Deaths:',nDeaths,
 
             # Recrossing goes here
             if self.recrossing:
@@ -124,6 +124,7 @@ class wavefunction:
            #for the additional births                                                   
             for n,(particle,weight) in enumerate(zip(x,weight_P_b)):
                 if weight>0: #i.e. the dead can't reproduce                              
+                    print 'weight for tiling:', weight, n,particle
                     if weight>10:
                         #this really shouldn't happen                                    
                         print 'weight is too big, resetting to 10'
@@ -131,13 +132,16 @@ class wavefunction:
                         weight=10
                     addBirthtot=addBirthtot+weight
                     temp=np.tile(particle,weight)
-                    temp_whoYaFrom=np.tile(whoYaFrom[p],weight)
+                    temp_whoYaFrom=np.tile(whoYaFrom[n],weight)
                     new_pop=np.concatenate((new_pop,temp))
                     new_pop_whoYaFrom=np.concatenate((new_pop_whoYaFrom,temp_whoYaFrom))
             next_gen=new_pop
             next_gen_whoYaFrom=new_pop_whoYaFrom
             #collect survivors and next generation                                       
             new_population=np.concatenate((survivors,next_gen))
+            whoYaFrom=np.concatenate((whoYaFrom[mask_survive],next_gen_whoYaFrom))
+            x=new_population
+
             self.currentPop=x.shape[0]
             if printCensus: print '. Births:',nBirths, ". Add' births: ", addBirthtot
 
@@ -145,12 +149,10 @@ class wavefunction:
             v_average=np.average(self.molecule.V(new_population))
             if not setV_ref:
                 v_ref=v_average+(self.alpha*(1-float(self.currentPop)/float(initialPop)))
-            if printCensus: print '(',N_size_step,'/',nSize,') v_ref',v_ref, '=', v_average,'+', (self.alpha*(1-float(N_size_step)/float(nSize)))
+            #if printCensus: print '(',self.currentPop,'/',initialPop,') v_ref',v_ref, '=', v_average,'+', (self.alpha*(1-float(self.currentPop)/float(initialPop)))
             vRefList.append(v_ref)
             population.append(self.currentPop)
 
-            whoYaFrom=np.concatenate((whoYaFrom[mask_survive],next_gen_whoYaFrom))
-            x=new_population
 
         for anc in whoYaFrom:
             descendants[anc]=descendants[anc]+1

HOHOH-groundState.py
@@ -1,6 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import DMCClusters as dmc
-Wfn=dmc.wavefunction('HOHOH', 5)
-print 'Before: Wfn.x',Wfn.x
-Wfn.propagate(Wfn.x,5)
-print 'After: Wfn.x',Wfn.x
+import time
+starttime=time.time()
+au2wn=219474.63
+averaged_vref=[]
+list_of_pop_list=[]
+N_size=5000
+N_steps=300
+nReps=10
+Wfn=dmc.wavefunction('HOHOH', N_size)
+initialx=Wfn.x
+print 'initial V', Wfn.molecule.V([initialx[0]])*au2wn
+
+for iwfn in range(nReps):
+    v_ref_list,pop_list,finalCoords,descendants=Wfn.propagate(initialx,N_steps,printCensus=False,initialPop=N_size)
+    endtime=time.time()
+    averaged_vref.append(np.average(np.array(v_ref_list)[N_steps-100:N_steps])*au2wn)
+    list_of_pop_list.append(pop_list)
+    plt.plot(np.array(v_ref_list)*au2wn)
+    initialx=finalCoords
+plt.show()
+print 'averaged v_ref:',averaged_vref
+print 'the average of average V_ref for the last 100 steps is',np.average(np.array(averaged_vref)), ' cm-1',
+print 'standard deviation', np.std(np.array(averaged_vref)), ' cm-1'
+print 'uncertainity is', (np.max(averaged_vref)-np.min(averaged_vref))/(2.0*np.sqrt(nReps))
+for pop_list in list_of_pop_list:
+    plt.plot(pop_list)
+plt.show()
+
+
+
+print 'that took', endtime-starttime, 'seconds and ', (endtime-starttime)/60.0 , 'minutes'
+
+
 print 'done!'