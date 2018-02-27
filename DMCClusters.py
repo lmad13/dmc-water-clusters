added the fixed node surface component to the algorithm.  This required a Rn calculation of the coordinate of interest as well as variables that corresponded to the existances of the excited state.  The Recrossing Correction was also added. Testing was implemented on the H3O2- molecule for the shared proton stretch.

DMCClusters.py
@@ -27,12 +27,17 @@ class wavefunction:
         self.set_dtau()
         self.mass=self.molecule.get_mass()
         self.sigma_dx=(2.0000*self.D*self.dtau/self.mass)**0.5
-        self.alpha=0.1/self.dtau
+        self.alpha=.25/self.dtau
 
-        self.recrossing=True if 'node' in potentialName else False
+        self.recrossing=False
 
+    def setNodalSurface(self,surfaceName,side='Both'):
+        self.surfaceName=surfaceName
+        self.side=side # Right: x>node
+        self.molecule.setNodalSurface(self.surfaceName,self.side)
+        self.recrossing=True
     def set_dtau(self):
-        self.dtau=100.0
+        self.dtau=10.0
         print 'dtau is ', self.dtau, 'imaginary atomic time units'
 
     def loadDict(self,fileName):
@@ -52,27 +57,29 @@ class wavefunction:
         return dx
 
     def propagate(self,x,nSteps,setV_ref=False,ConstantV_ref=0,printCensus=True,initialPop=0):
-        print 'ready to propagate for',nSteps, 'steps on x (shaped:',x.shape,') '
+        #print 'ready to propagate for',nSteps, 'steps on x (shaped:',x.shape,') '
+
         if initialPop==0:#if it is the default value of zero...it needs to be set.
             initialPop=x.shape[0]
+        self.currentPop=x.shape[0]
         if setV_ref:
             v_ref=ConstantV_ref
         else:
-            v_ref=np.average(self.molecule.V(x))#+(self.alpha*(1-float(N_size_step)/float(nSize)))
+            v_ref=np.average(self.molecule.V(x))+(self.alpha*(1-float(self.currentPop)/float(initialPop)))
 
-        self.currentPop=x.shape[0]
         population=[]
         population.append(self.currentPop)
-        print 'and x.size is', self.currentPop, 'out of',initialPop
-
+        
         vRefList=[]
         vRefList.append(v_ref)
 
         descendants=np.zeros((self.currentPop))
         whoYaFrom=np.arange(self.currentPop)
         
+        printRate=100
+
         for step in range(nSteps):
-            if printCensus: print 'step #',step, 
+            if step%printRate==0 and printCensus: print 'step #',step, 
             dx=self.diffuse()
             x=x+dx
             v=self.molecule.V(x)
@@ -86,34 +93,42 @@ class wavefunction:
             mask_survive=(Diff>0)
             nDeaths=np.sum(np.array(Diff<0).astype(int))
             survivors=x[mask_survive]
-            if printCensus: print 'Census: Current Population:',self.currentPop,'Deaths:',nDeaths,
+            if step%printRate==0 and printCensus: print 'Census: Current Population:',self.currentPop,'Deaths:',nDeaths,
 
-            # Recrossing goes here
+            # removal of all walkers that cross and a random selection of walkers that are too close to the node
             if self.recrossing:
-                #m2=2*(massO+2*massH)*massConversionFactor
-                #m1=massH*massConversionFactor
+                # m2=2*(massO+2*massH)*massConversionFactor
+                # m1=massH*massConversionFactor
                 newReducedMass=self.molecule.calcReducedmass(x)
                 oldDist=self.molecule.calcRn(x-dx)
                 newDist=self.molecule.calcRn(x)
-                oldReducedmass=calcReducedmass(x-dx)
+                crossed=(oldDist*newDist<0)
+
+                oldReducedMass=self.molecule.calcReducedmass(x-dx)
                 P_recrossDeath=np.exp(-2.0*(oldDist)*newDist*np.sqrt(oldReducedMass*newReducedMass)/self.dtau) ##mass is reduced mass!
                 #caution, this is clever, you are combining the probability of crossing and the probability of recrossing
-                if self.plotting:
-                    plt.scatter(x,P_recrossDeath)
-                    plt.show()
+                #if self.plotting:
+                #    plt.scatter(x,P_recrossDeath)
+                #    plt.show()
+                
                 Diff=N_r-P_recrossDeath
                 mask_survive_recross=(Diff>0)
                 tempRecrossCensus=np.sum(np.array(Diff<0).astype(int))
                 mask_survive=np.logical_and(mask_survive, mask_survive_recross)
                 survivors=x[mask_survive]            
 
-                if printCensus: print 'Deaths by recrossing: ',tempRecrossCensus
+                if step%printRate==0 and  printCensus: print 'Deaths by recrossing: ',tempRecrossCensus
             
-            #Creation of a random selection of walkers in the classically allowed region
+            #Creation of a random selection of walkers in the classically allowed region and who did not die by recrossing, so the survivors
+            
             P_exp_b=np.exp(-(v-v_ref)*self.dtau)-1.0
+            if self.recrossing:
+                P_exp_b[crossed]=0.00000  #if it crossed, the probability that it gives birth should be zero
+
             weight_P_b=P_exp_b.astype(int)
             P_b=P_exp_b-weight_P_b            #classically allowed region                            
             # P_b[np.logical_not(mask_survive)]=0.0                                       
+            
             Diff=N_r-P_b
             mask_b = (Diff<0)
             next_gen=x[mask_b]
@@ -121,38 +136,57 @@ class wavefunction:
             nBirths=np.sum(np.array(Diff<0).astype(int))
             addBirthtot=0
             new_pop=next_gen
+            
            #for the additional births                                                   
             for n,(particle,weight) in enumerate(zip(x,weight_P_b)):
                 if weight>0: #i.e. the dead can't reproduce                              
-                    print 'weight for tiling:', weight, n,particle
+                    #print 'weight for tiling:', weight, n
                     if weight>10:
                         #this really shouldn't happen                                    
                         print 'weight is too big, resetting to 10'
                         print x[n],v(n),'<',v_ref, -(v(n)-v_ref)
                         weight=10
                     addBirthtot=addBirthtot+weight
-                    temp=np.tile(particle,weight)
-                    temp_whoYaFrom=np.tile(whoYaFrom[n],weight)
+                    temp=np.array([particle])
+                    temp_whoYaFrom=np.array([whoYaFrom[n]])
+                    for i in range(weight-1):
+                        temp=np.concatenate((temp,np.array([particle])))
+                        temp_whoYaFrom=np.concatenate((temp_whoYaFrom,np.array([whoYaFrom[n]])))
+#                    temp=np.tile(particle,weight)
+#                    temp=np.reshape(temp,(weight,self.nAtoms,self.nDim))
+#                    temp_whoYaFrom=np.tile(whoYaFrom[n],weight)
+
                     new_pop=np.concatenate((new_pop,temp))
                     new_pop_whoYaFrom=np.concatenate((new_pop_whoYaFrom,temp_whoYaFrom))
+
             next_gen=new_pop
             next_gen_whoYaFrom=new_pop_whoYaFrom
             #collect survivors and next generation                                       
             new_population=np.concatenate((survivors,next_gen))
             whoYaFrom=np.concatenate((whoYaFrom[mask_survive],next_gen_whoYaFrom))
+            #print 'shapes for next cycle:',new_population.shape,whoYaFrom.shape
             x=new_population
-
+            
+            #let's make sure none of the walkers crossed to the other side...
+            position_on_coord=self.molecule.calcRn(x)
+            #print 'wrong side!', position_on_coord[(position_on_coord>0)],
+            #print 'from these array locations!',np.where(position_on_coord>0)
+#            if np.where(position_on_coord>0)[0].shape[0]>1:
+#               end
+            
             self.currentPop=x.shape[0]
-            if printCensus: print '. Births:',nBirths, ". Add' births: ", addBirthtot
+            if step%printRate==0 and printCensus: print 'Births:',nBirths, "Extra Births: ", addBirthtot,
 
             #readjust V_ref
             v_average=np.average(self.molecule.V(new_population))
+            
             if not setV_ref:
                 v_ref=v_average+(self.alpha*(1-float(self.currentPop)/float(initialPop)))
-            #if printCensus: print '(',self.currentPop,'/',initialPop,') v_ref',v_ref, '=', v_average,'+', (self.alpha*(1-float(self.currentPop)/float(initialPop)))
+            if step%printRate==0 and printCensus: print 'v_ref',v_ref, '=', v_average,'+', (self.alpha*(1-float(self.currentPop)/float(initialPop)))
             vRefList.append(v_ref)
             population.append(self.currentPop)
-
+            if self.currentPop<5:
+                print 'massive walker die off!', end
 
         for anc in whoYaFrom:
             descendants[anc]=descendants[anc]+1

HOHOH-SharedProtonFirstExcitedState.py
@@ -10,24 +10,32 @@ averaged_vref=[]
 list_of_pop_list=[]
 N_size=2000
 Wfn=dmc.wavefunction('HOHOH', N_size)
-
+Wfn.setNodalSurface('SharedProton',side='Both')
+    
 #Equilibration
 initialx=Wfn.x*1.1
+#initialx[:N_size/2]=Wfn.x[:N_size/2]*1.1
+#initialx[N_size/2:]=Wfn.x[N_size/2:]*-1.1
 equilibrationSteps=1000
-print 'initial V', Wfn.molecule.V([initialx[0]])*au2wn
+print 'initial V', Wfn.molecule.V(np.array([initialx[0]]))*au2wn
+print 'initial Rn', np.average(Wfn.molecule.calcRn(np.array([initialx[0]])))
 print 'equilibrating for ', equilibrationSteps, 'steps (',equilibrationSteps*Wfn.dtau,' au)'
 v_ref_equilibration,pop_list_equilibration,equilibratedCoordinates,descendants=Wfn.propagate(initialx,equilibrationSteps,printCensus=True,initialPop=N_size)
 inputx=equilibratedCoordinates
+
+print 'equilibrated Rn', np.average(Wfn.molecule.calcRn(equilibratedCoordinates))
 plt.figure(1)
 plt.subplot(311)
 plt.plot(np.arange(equilibrationSteps+1),np.array(v_ref_equilibration)*au2wn)
 plt.subplot(312)
 plt.plot(np.arange(equilibrationSteps+1),np.array(pop_list_equilibration))
+
 #Sampling of Psi
 propagationSteps=500
 descendantSteps=25
 nReps=5
-nRepsDW=5
+nRepsDW=2
+
 for iwfn in range(nReps):
     print '   REPETITION NUMBER: ', iwfn
     v_ref_list,pop_list,finalCoords,d=Wfn.propagate(inputx,propagationSteps,printCensus=True,initialPop=N_size)
@@ -44,7 +52,7 @@ for iwfn in range(nReps):
 
 
     Rn=Wfn.molecule.calcSharedProtonDisplacement(finalCoords)
-
+    print 'Rn for this section was', np.average(Rn)
     descendantWeights=np.zeros((finalCoords.shape[0]))
 
     for ides in range(nRepsDW):
@@ -61,11 +69,11 @@ for iwfn in range(nReps):
     AvePsi2Hist=AvePsi2Hist+Psi2Hist
     inputx=finalCoords
     
-plt.savefig('Vref-Pop-histogram-GroundState.png')
-plt.show()
 
+plt.show()
+plt.savefig('Vref-Pop-histogram-ExcState.png')
 print 'averaged v_ref:',averaged_vref
-print 'the average of average V_ref for the last 100 steps is',np.average(np.array(averaged_vref)), ' cm-1',
+print 'the average of average V_ref is',np.average(np.array(averaged_vref)), ' cm-1',
 print 'standard deviation', np.std(np.array(averaged_vref)), ' cm-1'
 print 'uncertainity is', (np.max(averaged_vref)-np.min(averaged_vref))/(2.0*np.sqrt(nReps))
 #for pop_list in list_of_pop_list:

molecularInfo.py
@@ -1,6 +1,10 @@
 import numpy as np
 import sys
 import os
+global massH
+global massO
+massH=1.00782503223
+massO=15.99491561957
 massConversionFactor=1.000000000000000000/6.02213670000e23/9.10938970000e-28#1822.88839 
 global Water
 Water={'H2O', 'water', 'HOH', 'h2o'}
@@ -21,7 +25,11 @@ class molecule:
             self.nAtoms=5
         self.pathDict=self.loadDict("paths.dict")
         self.potential=self.getPES()
-
+        self.surfaceName="GroundState"
+    def setNodalSurface(self,surfaceName,side):
+        self.surfaceName=surfaceName
+        self.side=side
+        self.state=1
     def getPES(self):
         sys.path.insert(0,self.pathDict["potentialPath"+self.name])
         import pes
@@ -41,6 +49,18 @@ class molecule:
     
     def V(self,x):
         v=np.array([self.potential(cart_in) for cart_in in x])
+        for n,pot_mol in enumerate(v):
+            if np.isnan(pot_mol):
+                print 'gosh, the ', n,'th is a problem:\n', x[n]
+
+        if self.surfaceName=='SharedProton':
+            r=self.calcRn(x)
+            if self.side=='Right':
+                v[(r<0)]=1000.00
+            elif self.side=='Left':
+                v[(r>0)]=1000.00
+            else:
+                donothing=1
         return v
 
 
@@ -50,7 +70,7 @@ class molecule:
         print 'there are ', self.nAtoms , 'atoms in ', self.name
         if self.name in DeprotonatedWaterDimer:
             coords=np.array([
-                    [   0.000000000000000 ,  0.000000000000000 ,  0.000000000000000 ],
+                    [   0.000000000000000 ,  0.000000000000000 ,  0.000000000000000],
                     [  -2.306185590098382 ,  0.000000000000000 ,  0.000000000000000],
                     [  -2.749724314110769 ,  1.765018349357672 ,  0.000000000000000],
                     [   2.306185590098382 ,  0.000000000000000 ,  0.000000000000000],
@@ -62,15 +82,15 @@ class molecule:
 
     def get_mass(self):
         mass=np.zeros((self.nAtoms))
-        massH=1.00782503223
-        massO=15.99491561957
         if self.name in DeprotonatedWaterDimer:
             mass=np.array([massH,massO,massH,massO,massH])
         return mass*massConversionFactor
 
     def calcReducedmass(self,x):
-        # for the shared proton stretch, page 42 in notebook #2                                                                   
-        if self.name in ProtonatedWaterDimer and self.state==1 and self.nodeCoord=='SharedProton':
+        # for the shared proton stretch, page 42 in notebook #2
+        massOamu=massO*massConversionFactor
+        massHamu=massH*massConversionFactor
+        if self.name in ProtonatedWaterDimer and self.state==1 and self.surfaceName=='SharedProton':
             ##COMwat1=1.0/massWater*(massO*x[:,0,:]+massH*(x[:,3,:]+x[:,4,:]))
             ##COMwat2=1.0/massWater*(massO*x[:,1,:]+massH*(x[:,5,:]+x[:,6,:]))
             ##U=x[:,2,:]-COMwat1                                              
@@ -80,8 +100,7 @@ class molecule:
 
             magU=np.sqrt(U[:,0]**2+U[:,1]**2+U[:,2]**2)
             magV=np.sqrt(V[:,0]**2+V[:,1]**2+V[:,2]**2)
-            massOamu=massO*conversionFactor
-            massHamu=massH*conversionFactor
+            
             costheta= np.diag(np.dot(U,V.T))/(magU*magV)
             #mass=1.0/(2.0*((1.0/(massOamu+massHamu+massHamu))+((1-costheta)/(massHamu))))                                        
             #corresponds to calcrncom                                                                                             
@@ -89,7 +108,7 @@ class molecule:
             mass=1.0/(2.0*((1.0/(massOamu))+((1-costheta)/(massHamu))))
 
             #Mass of water or Mass of O??                                                                                         
-        elif self.name in ProtonatedWaterDimer and self.state==1 and self.nodeCoord=='StretchAntiIn':
+        elif self.name in ProtonatedWaterDimer and self.state==1 and self.surfaceName=='StretchAntiIn':
 
             U=x[:,0,:]-x[:,3,:]
             V=x[:,0,:]-x[:,4,:]
@@ -104,8 +123,6 @@ class molecule:
             magV=np.sqrt(V[:,0]**2+V[:,1]**2+V[:,2]**2)
             costhetaWat2= np.diag(np.dot(U,V.T))/(magU*magV)
 
-            massOamu=massO*conversionFactor
-            massHamu=massH*conversionFactor
             g=( (1.0/massOamu)+(1.0/massHamu) )  +  1.0/2.0*((costhetaWat1/massOamu)+(costhetaWat2/massOamu))
 
             mass= 1.0/g
@@ -117,8 +134,6 @@ class molecule:
 
             magU=np.sqrt(U[:,0]**2+U[:,1]**2+U[:,2]**2)
             magV=np.sqrt(V[:,0]**2+V[:,1]**2+V[:,2]**2)
-            massOamu=massO*conversionFactor
-            massHamu=massH*conversionFactor
             costheta= np.diag(np.dot(U,V.T))/(magU*magV)
             mass=1.0/(2.0*((1.0/(massOamu))+((1-costheta)/(massHamu))))
 
@@ -134,29 +149,34 @@ class molecule:
             print 'why are you calculateing the reduced mass on the ground state?'  , end
 
         else:
-            print 'not implemented for ', self.name , 'and', self.state, 'and', self.nodeCoord,end
+            print 'not implemented for ', self.name , 'and', self.state, 'and', self.surfaceName,end
 
         return mass
+    
+    def calcSharedProtonDisplacement(self,x):
+        if self.name in ProtonatedWaterDimer:
+            r1=self.bondlength(x,atom1=2, atom2=1)
+            r2=self.bondlength(x,atom1=2, atom2=0)
+            return r2-r1
+        elif self.name in DeprotonatedWaterDimer:
+            r1=self.bondlength(x,atom1=0, atom2=1)
+            r2=self.bondlength(x,atom1=0, atom2=3)#ha                          
+            return r2-r1
+
+    def calcStretchAntiIn(self,x):
+        if self.name in ProtonatedWaterDimer:
+            r1=self.bondlength(x,atom1=0, atom2=3)
+            r2=self.bondlength(x,atom1=0, atom2=4)
+            r3=self.bondlength(x,atom1=1, atom2=5)
+            r4=self.bondlength(x,atom1=1, atom2=6)
+            return 0.5*(r1+r2-r3-r4)
 
     def calcRn(self,x):
-        if self.molecule in ProtonatedWaterDimer:
-            if self.nodeCoord=='SharedProton':
-                r1=self.bondlength(x,atom1=2, atom2=1)
-                r2=self.bondlength(x,atom1=2, atom2=0)
-                return r2-r1
+        if self.surfaceName=='SharedProton':
+            return self.calcSharedProtonDisplacement(x)
+        elif self.surfaceName=='StretchAntiIn':
+            return self.calcStretchAntiIn(x)
 
-            elif self.nodeCoord=='StretchAntiIn':
-                r1=self.bondlength(x,atom1=0, atom2=3)
-                r2=self.bondlength(x,atom1=0, atom2=4)
-                r3=self.bondlength(x,atom1=1, atom2=5)
-                r4=self.bondlength(x,atom1=1, atom2=6)
-                return 0.5*(r1+r2-r3-r4)
-
-        elif self.molecule in DeprotonatedWaterDimer:
-            if self.nodeCoord=='SharedProton':
-                r1=self.bondlength(x,atom1=0, atom2=1)
-                r2=self.bondlength(x,atom1=0, atom2=3)#ha                                                                         
-                return r2-r1
 
     def bondlength(self,pos,atom1=1,atom2=2):
         length=(pos[:,atom1,0]-pos[:,atom2,0])**2+(pos[:,atom1,1]-pos[:,atom2,1])**2+(pos[:,atom1,2]-pos[:,atom2,2])**2