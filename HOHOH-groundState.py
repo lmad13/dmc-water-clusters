48abd09ec3fdbc7341ccd972f4506b68f77c2163
Lindsey R. Madison <lindsey@D-69-91-183-26.dhcp4.washington.edu>
2018-03-06 17:04:49 -0800

    updated the ground state simulation to check the expectations of R and Mu and Mu**2, not just R**2

--- HOHOH-groundState.py
+++ HOHOH-groundState.py
@@ -28,6 +28,9 @@ list_of_pop_list=[]
 Wfn=dmc.wavefunction('HOHOH', N_size)
 Destination='ResultsH3O2/'
 GatherExpectationRn=[]
+GatherExpectationRn2=[]
+GatherExpectationMagMu=[]
+GatherExpectationMagMu2=[]
 #Equilibration
 initialx=Wfn.x*1.1
 
@@ -57,6 +60,7 @@ for iwfn in range(nReps):
     plt.plot(np.arange(iwfn*propagationSteps-1,(iwfn+1)*propagationSteps)+equilibrationSteps,np.array(pop_list))
 
     Rn=Wfn.molecule.calcSharedProtonDisplacement(finalCoords)
+    Dipole=Wfn.molecule.calcDipole(finalCoords)
 
     descendantWeights=np.zeros((finalCoords.shape[0]))
 
@@ -76,8 +80,15 @@ for iwfn in range(nReps):
     AvePsi2Hist=AvePsi2Hist+Psi2Hist
     inputx=finalCoords
 
-    GatherExpectationRn.append(np.sum(Rn*Rn*descendantWeights)/np.sum(descendantWeights))
-    print 'Rn:',GatherExpectationRn[-1]
+    GatherExpectationRn2.append(np.sum(Rn*Rn*descendantWeights)/np.sum(descendantWeights))
+    GatherExpectationRn.append(np.sum(Rn*descendantWeights)/np.sum(descendantWeights))
+    GatherExpectationMagMu.append(np.sum(np.linalg.norm(Dipole)*descendantWeights)/np.sum(descendantWeights))
+    GatherExpectationMagMu2.append(np.sum(np.linalg.norm(Dipole)**2*descendantWeights)/np.sum(descendantWeights))
+    print 'Rn^2:',GatherExpectationRn2[-1]
+    print 'Rn:  ',GatherExpectationRn[-1]
+    print 'Mu^2:',GatherExpectationMagMu2[-1]
+    print 'Mu:  ',GatherExpectationMagMu
+
 endtime=time.time()
 plt.savefig(Destination+'Vref-Pop-histogram-GroundState.png')
 plt.clf()
@@ -89,16 +100,46 @@ print 'uncertainity is', (np.max(averaged_vref)-np.min(averaged_vref))/(2.0*np.s
 print '--------   Rn   --------' 
 print '   Average:',np.average(GatherExpectationRn),'\n   Standard Deviation:',np.std(GatherExpectationRn)
 print '   Uncertainity:',(np.max(GatherExpectationRn)-np.min(GatherExpectationRn))/(2.0*np.sqrt(nReps))
+print '--------   Rn^2  --------' 
+print '   Average:',np.average(GatherExpectationRn2),'\n   Standard Deviation:',np.std(GatherExpectationRn2)
+print '   Uncertainity:',(np.max(GatherExpectationRn2)-np.min(GatherExpectationRn2))/(2.0*np.sqrt(nReps))
+print '--------   Rn   --------' 
+print '   Average:',np.average(GatherExpectationMagMu),'\n   Standard Deviation:',np.std(GatherExpectationMagMu)
+print '   Uncertainity:',(np.max(GatherExpectationMagMu)-np.min(GatherExpectationMagMu))/(2.0*np.sqrt(nReps))
+print '--------   Rn   --------' 
+print '   Average:',np.average(GatherExpectationMagMu2),'\n   Standard Deviation:',np.std(GatherExpectationMagMu2)
+print '   Uncertainity:',(np.max(GatherExpectationMagMu2)-np.min(GatherExpectationMagMu2))/(2.0*np.sqrt(nReps))
 #for pop_list in list_of_pop_list:
 #    plt.plot(pop_list)
 #plt.show()
 
-fileR2Data=open('R2Data-GroundState.data','a')
-fileR2Data.write('0   0 '+str(N_size)+'   '+str(propagationSteps)+'   '+str(nReps)+'   '+str(descendantSteps)+'   '+str(nRepsDW)+'      ')
-fileR2Data.write(str(np.average(averaged_vref))+'   '+str(np.std(averaged_vref))+'   '+str((np.max(averaged_vref)-np.min(averaged_vref))/(2.0*np.sqrt(nReps)))+'   ')
-fileR2Data.write(str(np.average(GatherExpectationRn))+'     '+str(np.std(GatherExpectationRn))+'     ')
-fileR2Data.write(str((np.max(GatherExpectationRn)-np.min(GatherExpectationRn))/(2.0*np.sqrt(nReps)))+'\n')
-fileR2Data.close()
+fileOutData=open('R2Data-GroundState.data','a')
+fileOutData.write('0   0 '+str(N_size)+'   '+str(propagationSteps)+'   '+str(nReps)+'   '+str(descendantSteps)+'   '+str(nRepsDW)+'      ')
+fileOutData.write(str(np.average(averaged_vref))+'   '+str(np.std(averaged_vref))+'   '+str((np.max(averaged_vref)-np.min(averaged_vref))/(2.0*np.sqrt(nReps)))+'   ')
+fileOutData.write(str(np.average(GatherExpectationRn2))+'     '+str(np.std(GatherExpectationRn2))+'     ')
+fileOutData.write(str((np.max(GatherExpectationRn2)-np.min(GatherExpectationRn2))/(2.0*np.sqrt(nReps)))+'\n')
+fileOutData.close()
+
+fileOutData=open('RData-GroundState.data','a')
+fileOutData.write('0   0 '+str(N_size)+'   '+str(propagationSteps)+'   '+str(nReps)+'   '+str(descendantSteps)+'   '+str(nRepsDW)+'      ')
+fileOutData.write(str(np.average(averaged_vref))+'   '+str(np.std(averaged_vref))+'   '+str((np.max(averaged_vref)-np.min(averaged_vref))/(2.0*np.sqrt(nReps)))+'   ')
+fileOutData.write(str(np.average(GatherExpectationRn))+'     '+str(np.std(GatherExpectationRn))+'     ')
+fileOutData.write(str((np.max(GatherExpectationRn)-np.min(GatherExpectationRn))/(2.0*np.sqrt(nReps)))+'\n')
+fileOutData.close()
+
+fileOutData=open('MagMuData-GroundState.data','a')
+fileOutData.write('0   0 '+str(N_size)+'   '+str(propagationSteps)+'   '+str(nReps)+'   '+str(descendantSteps)+'   '+str(nRepsDW)+'      ')
+fileOutData.write(str(np.average(averaged_vref))+'   '+str(np.std(averaged_vref))+'   '+str((np.max(averaged_vref)-np.min(averaged_vref))/(2.0*np.sqrt(nReps)))+'   ')
+fileOutData.write(str(np.average(GatherExpectationMagMu))+'     '+str(np.std(GatherExpectationMagMu))+'     ')
+fileOutData.write(str((np.max(GatherExpectationMagMu)-np.min(GatherExpectationMagMu))/(2.0*np.sqrt(nReps)))+'\n')
+fileOutData.close()
+
+fileOutData=open('MagMu2Data-GroundState.data','a')
+fileOutData.write('0   0 '+str(N_size)+'   '+str(propagationSteps)+'   '+str(nReps)+'   '+str(descendantSteps)+'   '+str(nRepsDW)+'      ')
+fileOutData.write(str(np.average(averaged_vref))+'   '+str(np.std(averaged_vref))+'   '+str((np.max(averaged_vref)-np.min(averaged_vref))/(2.0*np.sqrt(nReps)))+'   ')
+fileOutData.write(str(np.average(GatherExpectationMagMu2))+'     '+str(np.std(GatherExpectationMagMu2))+'     ')
+fileOutData.write(str((np.max(GatherExpectationMagMu2)-np.min(GatherExpectationMagMu2))/(2.0*np.sqrt(nReps)))+'\n')
+fileOutData.close()
 
 print 'that took', endtime-starttime, 'seconds and ', (endtime-starttime)/60.0 , 'minutes'
 
